662769217667c512bb137cd1c63ff15e2be07ce7
Matteo Menapace <hello@baddeo.com>
2015-03-08 18:28:40 +0000

    Shuffled re-sortable students list

---
+++ sessions/html5-boilerplate-random-student/data/students.js
@@ -0,0 +1,48 @@
+var students = 
+[
+	{
+		name: 'Harry',
+		surname: 'Foster',
+		imageId: '87496'
+	},
+	{
+		name: 'Callum',
+		surname: 'Holland',
+		imageId: '87498'
+	},
+	{
+		name: 'Margot',
+		surname: 'Mercier-Catherine',
+		imageId: '87499'
+	},
+	{
+		name: 'Burak',
+		surname: 'Ozdemir',
+		imageId: '87497'
+	},
+	{
+		name: 'Liam',
+		surname: 'Parker',
+		imageId: '87493'
+	},
+	{
+		name: 'Kimberly',
+		surname: 'Salazar-Lizalda',
+		imageId: '87995'
+	},
+	{
+		name: 'Innocent',
+		surname: 'Sekajja',
+		imageId: '87486'
+	},
+	{
+		name: 'Matt',
+		surname: 'Starr',
+		imageId: '87494'
+	},
+	{
+		name: 'Simeron',
+		surname: 'Taak',
+		imageId: '87495'
+	}
+]
---
+++ sessions/html5-boilerplate-random-student/js/main.js
@@ -0,0 +1,51 @@
+// from http://stackoverflow.com/a/24137301/4647178
+Array.prototype.getRandomElement = function () 
+{
+	return this[Math.floor((Math.random()*this.length))]
+}
+
+// from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
+Array.prototype.shuffle = function () 
+{
+	var currentIndex = this.length, 
+		temporaryValue, 
+		randomIndex
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) 
+	{
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex)
+		currentIndex -= 1
+
+		// And swap it with the current element.
+		temporaryValue = this[currentIndex]
+		this[currentIndex] = this[randomIndex]
+		this[randomIndex] = temporaryValue
+	}
+
+	return this
+}
+
+function studentTemplate(student)
+{
+	var html = '<li>'
+	html += '<img src="http://learn.rave.ac.uk/moodle/pluginfile.php/' + student.imageId + '/user/icon/ravebootstrap/f3">'
+	html += '<h3>' + student.name + '</h3>'
+	html += '<a class="button remove">Not here?</a>'
+	html += '</li>'
+	return html
+}
+
+function listStudents(students)
+{
+	var randomStudents = $.extend(true, [], students).shuffle(), // clone the students array and then shuffle it
+		$ul = $('ul#students')
+
+	randomStudents.forEach(function(student)
+	{
+		var li = studentTemplate(student)
+		$ul.append(li)
+	})
+}
